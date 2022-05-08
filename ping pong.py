9c999ef82deaa3ea2f1f176620aeeeae9570a40d
toughl1fe <104299639+toughl1fe@users.noreply.github.com>
2022-05-08 13:36:02 +0300

    Update ping pong.py

--- ping pong.py
+++ ping pong.py
@@ -2,7 +2,7 @@ from pygame import *
 from random import randint
 # Образец класса
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, player_width=50, player_high=50 ):
+    def __init__(self, player_image, player_x, player_y, player_speed, player_width, player_high):
         super().__init__()        
         self.image = transform.scale(image.load(player_image),(player_width, player_high))
         self.speed = player_speed
@@ -15,43 +15,62 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update(self):
         keys = key.get_pressed()
-        if keys[K_W] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_S] and self.rect.x < 730:
-            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 500:
+            self.rect.y += self.speed
 # Класс игрока 2
 class Player2(GameSprite):
     def update(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_DOWN] and self.rect.x < 730:
-            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 500:
+            self.rect.y += self.speed
         
 # Класс мяча
 class Ball(GameSprite):
     def update(self):
         pass  
 # Создание окна и счетчик кадров в секунду
-window = display.set_mode((900, 700))
+window = display.set_mode((800, 600))
 display.set_caption('Bored game')
-background = transform.scale(image.load('background.jpg'),(800, 600))
+background = transform.scale(image.load('background.png'),(800, 600))
+
 FPS = 160
 # Создание всех персонажей
-player = Player('racket2.jpg',10,5,5)
-player2 = Player2('racket2.jpg',895,5,5)
-ball = Ball('шаринган.jpg',50,1,1)
+player = Player('Rackets3.jpg',5,5,5, 50, 100)
+player2 = Player2('Rackets3.jpg',750,5,5, 50, 100)
+ball = Ball('pong3.png',375,275,1, 50, 50)
 
 
 # Причины остановки игры и показание всех персонажей
+speed_y = 3
+speed_x = 3
 finish = False
+clock = time.Clock()
 game = True
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+    if ball.rect.y > 600 - 50 or ball.rect.y < 0:
+        speed_y *= -1
+    if sprite.collide_rect(player, ball) or sprite.collide_rect(player2, ball):
+        speed_x *= -1
+    window.blit(background, (0,0))
+    player.reset()
+    player.update()
+    player2.reset()
+    player2.update()
+    ball.reset()
+    ball.update()
+    
 
-display.update()
-Clock.tick(FPS)
+    display.update()
+    clock.tick(FPS)
 
 
